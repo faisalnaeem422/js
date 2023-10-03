84bbb535a42607a7b1601aa42f9b16efacdf4a33
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-03 10:43:40 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser console
+
+console.log(3 + 3);
+
+
+//Different data types
+let name ="Faisal"
+let age = 18
+let isLoggedIn = false
+
+//Premitive data types
+//number => 2 to power of 53
+//bigint
+//string => "" or '' 
+//boolean => true or false
+//null => stand alone value  also data type
+//undefined => value not assigned
+//symbol => unique value
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
