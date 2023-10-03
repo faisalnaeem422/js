c9cc71b7ca40f30b37822fb5edb6e44c8d82f44e
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-03 10:19:00 +0000

    different ways to create variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144552;
+let accountEmail = "faisalnaeem@gmail.com"
+var accountPassword = "12345"
+accountCity = "Islamabad"
+let accountState
+// accountId = 12312; //not allowed
+
+accountEmail ="accountemail@gmail.com"
+accountPassword= "43214"
+accountCity= "Rawalpindi"
+
+/* Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
