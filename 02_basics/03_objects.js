f2d20f57ace3beeee5da7e3d6d80301db6f7950d
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-05 10:21:35 +0000

    in depth  js objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,10 +1,14 @@
 //singleton
 
 //object literals
+
+const mySymbol = Symbol("Key1")
+
 const jsUser={
     //object store key value pair
     name:"Faisal",
     "full name":"Faisal Naeem",
+    [mySymbol]:"myKey1",
     age:26,
     location:"Islamabad",
     email:"Faisal@google.com",
@@ -12,6 +16,24 @@ const jsUser={
     lastLoginDays:["Monday","Thursday"]
 }
 
-console.log(jsUser.email);
-console.log(jsUser["full name"]);
-console.log(jsUser["email"]);
+//change value of object keys with objectName.key = "your new value"
+jsUser.email ="Faisal@chatgpt.com"
+
+//to freeze object that no one can change it
+// Object.freeze(jsUser)
+//now object name value can't change
+jsUser.name = "Muhammad Faisal Naeem "
+// console.log(jsUser.email);
+// console.log(jsUser["full name"]);
+// console.log(jsUser["email"]);
+// console.log(typeof jsUser[mySymbol]);
+
+jsUser.greeting = function(){
+    console.log(`Welcome to js series ${this["full name"]}`);
+}
+jsUser.greetingTwo = function(){
+    console.log(`Welcome to js series, ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
