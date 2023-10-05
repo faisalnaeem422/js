c9661ab76b7d63ec18f7c3eda01e4c8060cc24cf
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-05 10:09:56 +0000

    objects in javascript

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+//singleton
+
+//object literals
+const jsUser={
+    //object store key value pair
+    name:"Faisal",
+    "full name":"Faisal Naeem",
+    age:26,
+    location:"Islamabad",
+    email:"Faisal@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Thursday"]
+}
+
+console.log(jsUser.email);
+console.log(jsUser["full name"]);
+console.log(jsUser["email"]);
