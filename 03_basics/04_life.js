c9a6e975328f69574a9f0560d966be05b5794cdf
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-10 10:40:31 +0000

    named IIFE and Simple IIFE

--- 03_basics/04_life.js
+++ 03_basics/04_life.js
@@ -2,11 +2,13 @@
 
 //when you want to immediately execute a function put (function)()
 (function chai(){
+    //named IIFE
     console.log("DB Connected")
 })(); //here semicolon is important
 
 // you can also use arrow function for these
 
 ((name)=>{
+    //Simple IIFE
     console.log(`Second Db Connected ${name}`);
 })("Mongodb")
