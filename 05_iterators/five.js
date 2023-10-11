1594e9cc4b4f15a9673ef314910d3b3af8a2fe1c
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-11 06:45:57 +0000

    forEach with array of object

--- 05_iterators/five.js
+++ 05_iterators/five.js
@@ -1,5 +1,5 @@
 //for each loop
-const coding =["js","css","rb","cpp","py","java"];
+const coding = ["js", "css", "rb", "cpp", "py", "java"];
 
 //For each is a high order function
 // coding.forEach(element => {
@@ -15,8 +15,38 @@ const coding =["js","css","rb","cpp","py","java"];
 // you can use another function as call back
 //create a fucntion and just pass its reference to foreach as callbak
 
-function codingLanguages(item){
-    console.log(item);
+function codingLanguages(item) {
+  console.log(item);
 }
 
-coding.forEach(codingLanguages)
+// coding.forEach(codingLanguages)
+
+//Other parameter
+// coding.forEach((item , index, arr)=>{
+//     console.log(item , index , arr);
+// });
+
+//Objects with in the array
+const myCoding = [
+  {
+    langName: "Javascript",
+    langExtension: "js",
+  },
+  {
+    langName: "Java",
+    langExtension: "java",
+  },
+  {
+    langName: "Python",
+    langExtension: "py",
+  },
+  {
+    langName: "Ruby",
+    langExtension: "rb",
+  },
+];
+
+
+myCoding.forEach(lang => {
+    console.log(lang.langName , lang.langExtension); 
+});
