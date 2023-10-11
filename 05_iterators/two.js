736c9e6bb186722a57cc0c04cddf8d837537439f
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-11 05:56:59 +0000

    while and do-while loop

---
+++ 05_iterators/two.js
@@ -0,0 +1,25 @@
+let index = 0;
+
+//simple while loop
+// while(index <=20){
+//     console.log(`value of index is ${index}`);
+//     index = index+ 2
+// }
+
+
+//populate array with while loop
+let myArray = ["Flash" , "Batman" ,"Superman" ,"Spiderman"];
+let arr = 0;
+
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+
+//do while loop
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10)
