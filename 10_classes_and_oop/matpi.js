c0fc0c5e1341108824170f9d13201c09705ddbd7
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-31 04:43:55 +0000

    Objects in js

---
+++ 10_classes_and_oop/matpi.js
@@ -0,0 +1,17 @@
+// const descriptor  =Object.getOwnPropertyDescriptor(Math , "PI");
+
+// console.log(descriptor);
+
+const chai ={
+    name:'ginger chai',
+    price:'350',
+    isAvailable:true
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+Object.defineProperty(chai , 'name' , {
+    writable:false,
+    enumerable:false
+})
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
