c1184296955a17c5e60535dddd4fd04a17919c1d
faisalnaeem422 <35003423+faisalnaeem422@users.noreply.github.com>
2023-10-31 05:08:42 +0000

    advance objects in js

--- 10_classes_and_oop/matpi.js
+++ 10_classes_and_oop/matpi.js
@@ -4,14 +4,30 @@
 
 const chai ={
     name:'ginger chai',
-    price:'350',
-    isAvailable:true
+    price:350,
+    isAvailable:true,
+
+    orderChai : function(){
+        console.log("Order kro chai");
+    }
 }
 
-console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+// console.log(Object.getOwnPropertyDescriptor(chai , "name"));
 
 Object.defineProperty(chai , 'name' , {
-    writable:false,
+    //writable:false,
     enumerable:false
 })
-console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+// console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+//can not redefine property
+// Object.defineProperty(Math , "PI" ,{
+//     writable:true,
+//     enumerable:true
+// })
+// console.log(Object.getOwnPropertyDescriptor(Math , "PI"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if(typeof value !== 'function')
+   console.log(`${key}  : ${value}`);
+}
